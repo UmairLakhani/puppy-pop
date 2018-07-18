16db51773f051ac16c41da698c9b9af4cc84f80f
UmairLakhani <mairlakhani@gmail.com>
2018-07-19 01:52:37 +0500

    first

---
+++ music/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Album(models.Model):
+    artist = models.CharField(max_length=200)
+    album_title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+    album_logo = models.FileField()
+
+    def get_absolute_url(self):
+        return reverse('music:detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.artist + ' - ' + self.album_title
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    song_title = models.CharField(max_length=200)
+    audio_file = models.FileField(default='')
+    is_fav = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.song_title
---
+++ music/views.py
@@ -0,0 +1,70 @@
+from django.views import generic
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic import View
+from .models import Album
+from . form import UserForm
+
+
+class IndexView(generic.ListView):
+    model = Album
+    template_name = 'music/index.html'
+
+
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
+
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registration-form.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+
+                if user.is_active:
+                    login(request, user)
+                    return redirect('music:index')
+
+        return render(request, self.template_name, {'form': form})
+
+
+
+
+
+
