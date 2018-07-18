ba70dfb4be7b4b3424a9823759abd9b46b424a66
Umair Lakhani <mairlakhani@gmail.com>
2018-07-19 02:06:09 +0500

    Update settings.py

--- firstPro/settings.py
+++ firstPro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'azhqzmkvp54q581g7r-g9qvp9axma&!*_kt&6@dw@rq5x-+)=i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
